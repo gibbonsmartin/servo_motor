7c396c025ab46f52a17f46a089bd2e58a6471548
gibbonsmartin <32617706+gibbonsmartin@users.noreply.github.com>
2021-02-05 21:46:38 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -29,81 +29,27 @@ function do_motor(velocity : number)
     }
 }
 
-function on_button_pressed_b() {
-    pins.analogWritePin(AnalogPin.P12, 1023*speed/10)
-    pins.analogWritePin(AnalogPin.P8, 0)
-}
-
-function on_button_pressed_f() {
-    pins.analogWritePin(AnalogPin.P12, 0)
-    pins.analogWritePin(AnalogPin.P8, 1023*speed/10)
-}
-
-function on_button_pressed_s() {
-    pins.analogWritePin(AnalogPin.P12, 0)
-    pins.analogWritePin(AnalogPin.P8, 0)
-}
-function on_button_pressed_u() {
-    servos.P1.setAngle(10)
-}
-
-function on_button_pressed_d() {
-    servos.P1.setAngle(170)
-}
-
-function on_button_pressed_l() {
-    servos.P2.setAngle(10)
-}
-
-function on_button_pressed_r() {
-    servos.P2.setAngle(170)
-}
-
-function refresh_speed()
-{
-    if (direction == "s") {
-        on_button_pressed_s()
-    } else if (direction == "f") {
-        on_button_pressed_f()
-    } else if (direction == "b") {
-        on_button_pressed_b()
-    }
-}
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function on_uart_data_received() {
     let data = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
-    if (data == "l") {
-        on_button_pressed_l()
-    } else if (data == "r") {
-        on_button_pressed_r()
-    } else if (data == "s") {
-        on_button_pressed_s()
-        direction = "s"
-    } else if (data == "f") {
-        on_button_pressed_f()
-        direction = "f"
-    } else if (data == "b") {
-        on_button_pressed_b()
-        direction = "b"
-    } else if (data == "u") {
-        on_button_pressed_u()
-    } else if (data == "d") {
-        on_button_pressed_d()
-    }else{
-        speed = parseInt(data)
-        refresh_speed()
-    }
+
     basic.showString(data)
+    switch (data.substr(0,1)){
+        case "S":
+            switch (data.substr(1,1)){
+                case "0":
+                    do_servo(Servos.SERVO_1, parseInt(data.substr(2)) )
+                break;
+                case "1":
+                    do_servo(Servos.SERVO_1, parseInt(data.substr(2)) )
+                break;
+                default:
+                break;
+            }
+        break;
+        case "M":
+            do_motor(parseInt(data.substr(1)))
+        break;
+        default:
+        break;
+    }
 })
-speed = 5
-direction = "s"
-input.onButtonPressed(Button.A, on_button_pressed_f)
-input.onButtonPressed(Button.B, on_button_pressed_b)
-pins.servoSetPulse(AnalogPin.P2, 1500)
-pins.servoSetPulse(AnalogPin.P1, 1500)
-pins.analogWritePin(AnalogPin.P12, 0)
-pins.analogWritePin(AnalogPin.P8, 0)
-pins.analogSetPeriod(AnalogPin.P12, 20000)
-pins.analogSetPeriod(AnalogPin.P8, 20000)
-on_button_pressed_u()
-on_button_pressed_r()
-on_button_pressed_s()
